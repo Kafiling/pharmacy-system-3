69e48072edd0630306466184daa53cb37053630a
peemsutthi <peemsutthi@gmail.com>
2025-04-17 12:25:59 +0700

    feat:sort-by-id

--- app/(dashborad)/inventory/page.tsx
+++ app/(dashborad)/inventory/page.tsx
@@ -226,7 +226,12 @@ export default function InventoryPage() {
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[100px]">ID</TableHead>
+                <TableHead className="w-[100px]">
+                  <div className="flex items-center cursor-pointer" onClick={() => handleSort("id")}>
+                    ID
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                  </div>
+                </TableHead>
                 <TableHead>
                   <div className="flex items-center cursor-pointer" onClick={() => handleSort("name")}>
                     Name
